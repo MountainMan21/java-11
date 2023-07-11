82f2441620bdd4a19aab23ef06dabb77cb205221
Ashir <122673237+MountainMan21@users.noreply.github.com>
2023-07-11 05:24:10 +0500

    using of Static

---
+++ java_link.java
@@ -0,0 +1,32 @@
+package java_concepts_revision;
+
+public class java_link {
+
+//       using static class variable outside the class
+//	Constructor is always used when object of a class is created.
+		
+	private String name ;
+	public static final String Best_Programmer = " first in java";
+	
+
+
+public java_link(String name) {
+	super();
+	this.name = name;
+}
+
+
+
+
+@Override
+public String toString() {
+	return "java_link [name=" + name + "]";
+}
+
+
+
+
+}
+
+
+
--- jave_concepts_revise.java
+++ jave_concepts_revise.java
@@ -5,55 +5,14 @@ public class jave_concepts_revise {
 	
 		public static void main(String[] args) {
 			
-			// boolean and && 
-			boolean have_car = true;
-			boolean known_how_to_drive = true;
-			boolean can_drive = false;
+		java_link java_link1 = new java_link ("programmer1");
+		java_link java_link2 = new java_link ("programmer2");
+		
+		System.out.println(java_link1);
+			System.out.println(java_link2);
 			
-			if(have_car) {
-				if(known_how_to_drive) {
-					can_drive = true;
-				}
-			}
-			if (have_car && known_how_to_drive) {
-				can_drive = true;
-			}
-			System.out.println(can_drive);
-			
-			can_drive = have_car&&known_how_to_drive ? true:false;
-			
-			
-			// now we use boolean OR.
-			
-			boolean buy_car= false;
-			boolean rent_car=false;
-			boolean drive_car=false;
-			
-			drive_car = buy_car || rent_car;
-			System.out.println(drive_car);
-			
-			
-			// complex boolean expression 
-			
-			boolean license_have = false;
-			boolean have_ID = false;
-			boolean over_speeding_ticket= true;
-			if ((license_have || have_ID) && over_speeding_ticket ) {
-				System.out.println("you have fine");
-			}
-			else{
-				System.out.println("sent to jail");
-			}
-			
-			boolean	fine = license_have || have_ID;
-			if(fine && over_speeding_ticket)
-			{
-				System.out.println("you have 500 fine ticket");
-			}
-			else {
-				System.out.println("jail time");
-			}
-			
+			System.out.println(java_link.Best_Programmer);
+			System.out.println(Math.PI);
 		}
 	}
 
