4ef1ea3bff34a708ce668b90be7ccfa6e0daa16e
Ashir <122673237+MountainMan21@users.noreply.github.com>
2023-07-20 14:57:11 +0500

    Add files via upload
    Constructors and their use

---
+++ RockPaperScissors/src/Overloading_and_chaing_constructure_and_Superclass/src/Chaining_constructure/Person.java
@@ -0,0 +1,32 @@
+package Chaining_constructure;
+
+public class Person {
+
+	private int age;
+	private String name;
+	private static final String Name = "Ashir";
+	
+	
+	public Person() {
+		this(Name,0);
+	}
+	
+	public Person(String name) {
+		this(name,0);
+	}
+	
+	public Person(int age,String Name) {
+		this(Name,age);
+	}
+	public   Person(String name,int age) {
+		this.name=name;
+		this.age=age;
+		System.out.println("main cons");
+	}
+
+	@Override
+	public String toString() {
+		return name + "," + age ;
+	}
+	
+}
---
+++ RockPaperScissors/src/Overloading_and_chaing_constructure_and_Superclass/src/Super_class/app.java
@@ -0,0 +1,10 @@
+package Super_class;
+
+public class app {
+
+	public static void main(String[] args) {
+		employee employe = new employee("Kaleem");
+		System.out.println(employe);
+	}
+
+}
