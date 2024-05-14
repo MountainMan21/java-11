5138aa409d77572ca0a834759f40ff2836e39966
Ashir <122673237+MountainMan21@users.noreply.github.com>
2024-05-15 04:29:04 +0500

    Add files via upload

---
+++ JavaSwing/src/gui/Toolbar.java
@@ -0,0 +1,31 @@
+package gui;
+
+import java.awt.Color;
+
+
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class Toolbar extends JToolBar {
+
+	private static final long serialVersionUID = 1L;
+	private ColorChangeListener colorChanger;
+	
+	public Toolbar() {
+
+		final JButton redButton = new JButton("Red");
+		final JButton blueButton = new JButton("Blue");
+
+		redButton.addActionListener(e->colorChanger.changeColor(Color.red));
+		blueButton.addActionListener(e->colorChanger.changeColor(Color.blue));
+
+		add(redButton);
+		add(blueButton);
+	}
+
+	public void setColorChanger(ColorChangeListener colorChanger) {
+		this.colorChanger = colorChanger;
+	}
+	
+	
+}
---
+++ JavaSwing/src/module-info.java
@@ -0,0 +1,5 @@
+
+module JavaSwing {
+	
+	requires java.desktop;
+}
